fad765c630278d28feedca63d4ab89e76604fd56
mwkrier <michaelwkrier@gmail.com>
2018-08-14 11:52:10 -0400

    mwkrierportfolio

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# mwkrierportfolio
+# personalwebsite
---
+++ assets/script.js
@@ -0,0 +1,7 @@
+function showBlog1() {
+    $(".noShow").css({display: "none"});
+    $('.learn-code').css({display: "block"});
+    console.log("clicked");
+};
+
+
