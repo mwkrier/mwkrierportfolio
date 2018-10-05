1cfd420771c1e2aa459e3d6c1d53614ea32b617d
mwkrier <michaelwkrier@gmail.com>
2018-10-05 17:14:55 -0400

    mwkrierportfolio

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,13 @@ $("#collapseTwo").on('click', function(){
     $("#collapseTwo").css("show");
 })
 
+$("#portfolioButton").on('click', function(){
+    $('#portfolio').css("display", "block");
+    $('#hidden').css("display", "none");
+});
+
+$("#about-res").on('click', function(){
+    $('#hidden').css("display", "block");
+})
+
 
